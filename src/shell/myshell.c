fe5b8171f880d95f08b91c6893279c8ea83953f6
qihangzp@gmail.com <qihangzp@gmail.com>
2017-02-08 00:32:52 +0800

    working on parse_cmd

--- src/shell/myshell.c
+++ src/shell/myshell.c
@@ -14,12 +14,35 @@ void main(void) {
         printf("%s", cmd_head);
         
         // read user input
-        char one_line[MAX_LINE_LEN];
-        fgets(one_line, MAX_LINE_LEN, stdin);
+        char one_line[MAX_LINE_LEN] = "grep \"aaaa\" test.txt";
+        // fgets(one_line, MAX_LINE_LEN, stdin);
 
+        // one_line = "grep \"aaaa\" test.txt";
+        printf("sizeof one_line == %d", sizeof(one_line));
         // parse user input
+        int parsed_cmd_len;
+        char** parsed_cmd;
+
         
         printf("%s\n", one_line);
+
+        break;
     }
+}
+
+/*
+ * grep "aaaa" test.txt -> ["grep", "\"aaaa\"", "text.txt"]
+ */
+void parse_cmd(char* one_line, char** parsed_cmd, int* parsed_cmd_len) {
+    int len = 3;
+    *parsed_cmd_len = len;
+    parsed_cmd = malloc(sizeof(char* a) * len);
+
+    char* cmd0 = "grep";
+    *parsed_cmd = malloc(strlen(cmd0));
+    char* cmd1 = "\"aaaa\"";
+    
+    (*parsed_cmd) + 1 = 
+
 }
 
