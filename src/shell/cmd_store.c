dc36489071ec32428859b0f029afb0ff96355677
qihang <qihangzp@gmail.com>
2017-04-13 00:08:11 +0800

    cmocka works now!
    Next step: impl add_cmd

--- src/shell/cmd_store.c
+++ src/shell/cmd_store.c
@@ -1,9 +1,9 @@
 #include "cmd_store.h"
 #include <stdlib.h>
 
-struct CmdStore* cmd_store_builder(void) {
-    CmdStore* ret = malloc(sizeof(CmdStore));
-    ret->cmds = malloc(sizeof(int[0]) * CMD_STORE_INIT_SLOTS);
+struct CmdStore* cmd_store_builder() {
+    struct CmdStore* ret = (struct CmdStore*) malloc(sizeof(struct CmdStore));
+    ret->cmds = (cmd_p) malloc(sizeof(int*) * CMD_STORE_INIT_SLOTS);
     ret->free_space = 0;
     ret->init_slots = CMD_STORE_INIT_SLOTS;
     ret->max_slots = CMD_STORE_MAX_SLOTS;
